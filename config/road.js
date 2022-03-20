36654e1fd1711636f4995ec71707fc6304b73185
phamlam2k <mailam1309@gmail.com>
2022-03-20 22:41:00 +0700

    finish the prj

---
+++ config/road.js
@@ -0,0 +1,9 @@
+function createCube(size, posX, rotY = 0, color = 0xfbc851){
+    const geometry = new THREE.BoxGeometry( size.w, size.h, size.d )
+    const material = new THREE.MeshBasicMaterial( { color } )
+    const cube = new THREE.Mesh( geometry, material )
+    cube.position.set(posX, 0, 0)
+    cube.rotation.y = rotY
+    scene.add( cube )
+    return cube
+}
--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
 
 const renderer = new THREE.WebGLRenderer()
-renderer.setSize( window.innerWidth, window.innerHeight )
-document.body.appendChild( renderer.domElement )
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-const light = new THREE.AmbientLight( 0xffffff )
-scene.add( light )
+const light = new THREE.AmbientLight(0xffffff)
+scene.add(light)
 
-const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.3 );
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.3);
 directionalLight.castShadow = true
-scene.add( directionalLight )
-directionalLight.position.set( 0, 1, 1 )
+scene.add(directionalLight)
+directionalLight.position.set(0, 1, 1)
 
 camera.position.z = 5
-renderer.setClearColor( 0xB7C3F3, 1 )
+renderer.setClearColor(0xB7C3F3, 1)
 
 const loader = new THREE.GLTFLoader()
 let doll
@@ -25,47 +26,55 @@ const text = document.querySelector('.text')
 
 const startBtn = document.querySelector('.start-btn')
 
-loader.load( './model/scene.gltf', function ( gltf ){
-    scene.add( gltf.scene )
+// Create the doll.
+loader.load('./model/scene.gltf', function (gltf) {
+    scene.add(gltf.scene)
     doll = gltf.scene
     gltf.scene.position.set(.2, -1, .2)
     gltf.scene.scale.set(0.4, 0.4, 0.4)
     startBtn.innerText = "start"
 })
 
-function lookBackward(){
-    gsap.to(doll.rotation, {duration: .45, y: -3.15})
+function lookBackward() {
+    gsap.to(doll.rotation, {
+        duration: .45,
+        y: -3.15
+    })
     setTimeout(() => dallFacingBack = true, 150)
 }
-function lookForward(){
-    gsap.to(doll.rotation, {duration: .45, y: 0})
+
+function lookForward() {
+    gsap.to(doll.rotation, {
+        duration: .45,
+        y: 0
+    })
     setTimeout(() => dallFacingBack = false, 450)
 }
 
-function createCube(size, posX, rotY = 0, color = 0xfbc851){
-    const geometry = new THREE.BoxGeometry( size.w, size.h, size.d )
-    const material = new THREE.MeshBasicMaterial( { color } )
-    const cube = new THREE.Mesh( geometry, material )
-    cube.position.set(posX, 0, 0)
-    cube.rotation.y = rotY
-    scene.add( cube )
-    return cube
-}
+// Create the road.
+createCube({
+    w: start_position * 2 + .21,
+    h: 1.5,
+    d: 1
+}, 0, 0, 0xe5a716).position.z = -1
+createCube({
+    w: .2,
+    h: 1.5,
+    d: 1
+}, start_position, -.4)
+createCube({
+    w: .2,
+    h: 1.5,
+    d: 1
+}, end_position, .4)
 
-//Creating runway
-createCube({w: start_position * 2 + .21, h: 1.5, d: 1}, 0, 0, 0xe5a716).position.z = -1
-createCube({w: .2, h: 1.5, d: 1}, start_position, -.4)
-createCube({w: .2, h: 1.5, d: 1}, end_position, .4)
 
-async function delay(ms){
-    return new Promise(resolve => setTimeout(resolve, ms))
-}
 
+// Create the player.
 const player1 = new Player("Player 1", .25, .3, 0xD1FFC6)
 const player2 = new Player("Player 2", .25, -.3, 0xFFCFD2)
 
-const players = [
-    {
+const players = [{
         player: player1,
         key: "ArrowUp",
         name: "Player 1"
@@ -76,12 +85,18 @@ const players = [
         name: "Player 2"
     }
 ]
-//musics
+
+// Function to sovle asynchronous.
+async function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+//Get the musics.
 const bgMusic = new Audio('./music/bg.mp3')
 const winMusic = new Audio('./music/win.mp3')
 const loseMusic = new Audio('./music/lose.mp3')
 bgMusic.loop = true
-async function init(){
+async function init() {
     await delay(500)
     text.innerText = "Starting in 4"
     await delay(500)
@@ -97,13 +112,21 @@ async function init(){
     start()
 }
 
-function start(){
+function start() {
     gameStat = "started"
-    const progressBar = createCube({w: 8, h: .1, d: 1}, 0, 0, 0xebaa12)
+    const progressBar = createCube({
+        w: 8,
+        h: .1,
+        d: 1
+    }, 0, 0, 0xebaa12)
     progressBar.position.y = 3.35
-    gsap.to(progressBar.scale, {duration: TIME_LIMIT, x: 0, ease: "none"})
+    gsap.to(progressBar.scale, {
+        duration: TIME_LIMIT,
+        x: 0,
+        ease: "none"
+    })
     setTimeout(() => {
-        if(gameStat != "ended"){
+        if (gameStat != "ended") {
             text.innerText = "Time Out!!!"
             loseMusic.play()
             gameStat = "ended"
@@ -112,48 +135,49 @@ function start(){
     startDall()
 }
 
-async function startDall(){
-   lookBackward()
-   await delay((Math.random() * 1500) + 1500)
-   lookForward()
-   await delay((Math.random() * 750) + 750)
-   startDall()
+async function startDall() {
+    lookBackward()
+    await delay((Math.random() * 1500) + 1500)
+    lookForward()
+    await delay((Math.random() * 750) + 750)
+    startDall()
 }
 
-
 startBtn.addEventListener('click', () => {
-    if(startBtn.innerText == "START"){
+    if (startBtn.innerText == "START") {
         init()
         document.querySelector('.modal').style.display = "none"
     }
 })
 
-function animate(){
-    renderer.render( scene, camera )
+function animate() {
+    renderer.render(scene, camera)
     players.map(player => player.player.update())
-    if(gameStat == "ended") return
-    requestAnimationFrame( animate )
+    if (gameStat == "ended") return
+    requestAnimationFrame(animate)
 }
 animate()
 
-window.addEventListener( "keydown", function(e){
-    if(gameStat != "started") return
+// Key status of window.
+window.addEventListener("keydown", function (e) {
+    if (gameStat != "started") return
     let p = players.find(player => player.key == e.key)
-    if(p){
+    if (p) {
         p.player.run()
     }
 })
 
-window.addEventListener( "keyup", function(e){
+window.addEventListener("keyup", function (e) {
     let p = players.find(player => player.key == e.key)
-    if(p){
+    if (p) {
         p.player.stop()
     }
 })
 
-window.addEventListener( 'resize', onWindowResize, false )
-function onWindowResize(){
+window.addEventListener('resize', onWindowResize, false)
+
+function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
-    renderer.setSize( window.innerWidth, window.innerHeight )
+    renderer.setSize(window.innerWidth, window.innerHeight)
 }
